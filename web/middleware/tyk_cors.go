e450587ca1f70e642e9ce277544d06bec01e8500
Martynas Adomaitis <martynas.adomaitis@monetha.io>
2019-03-13 12:39:38 +0200

    Disabled OPTIONS handling in middleware

--- web/middleware/tyk_cors.go
+++ web/middleware/tyk_cors.go
@@ -7,7 +7,6 @@ import (
 )
 
 // TykCors is a middleware that handles CORS headers in a fashion that works with Tyk
-// Only handles OPTIONS calls, because Tyk handles the rest
 // NOTE: Use this middleware when you have enabled CORS handling in Tyk and "Options passthrogh" is enabled
 type TykCors struct {
 	cors *cors.Cors
@@ -23,15 +22,16 @@ func NewTykCors(c *cors.Cors) *TykCors {
 // Handler apply the CORS specification on the request, and add relevant CORS headers
 // as necessary
 func (tc *TykCors) Handler(h http.Handler) http.Handler {
-	corsHandlerFunc := tc.cors.Handler(h)
+	// corsHandlerFunc := tc.cors.Handler(h)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		// Handle only OPTIONS, because rest is handled by Tyk
-		if r.Method == http.MethodOptions {
-			corsHandlerFunc.ServeHTTP(w, r)
-			return
-		}
+
+		// if (TODO: not handled by tyk (Tyk might add some custom header)) {
+		// corsHandlerFunc.ServeHTTP(w, r)
+		// return
+		// }
 
 		h.ServeHTTP(w, r)
 	})
